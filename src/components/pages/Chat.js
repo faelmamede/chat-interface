695f695776ae27f6a49134e2d7d1457ca6ef4814
Bruna <brunatpicoli@outlook.com>
2022-05-12 19:30:11 -0300

    finalização da parte estática do chat

--- src/components/pages/Chat.js
+++ src/components/pages/Chat.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import userImg from '../../images/user.png';
 
 import './Chat.css'
 
 function Chat(props) {
     return (
         <main className='chat-container'>
-            <h1>Chat</h1>
+            <section className='user-screen'>
+                <div className='user-info'>
+                    <img src={userImg} alt="User" />
+                    <h2>User 1</h2>
+                </div>
+                <section className='user-messages'>
+                    
+                </section>
+            </section>
+            <section className='user-keyboard'>
+                <input type="text" />
+                <button></button>
+            </section>
         </main>
     )
 }
