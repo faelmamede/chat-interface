f62b3322e02542f2ad681151343ceb6588fa1010
Bruna <brunatpicoli@outlook.com>
2022-05-12 16:04:16 -0300

    finalização da parte estática da nav

--- src/components/template/Nav.js
+++ src/components/template/Nav.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import userImg from '../../images/user.png';
 
 import './Nav.css'
+
+function activeUser(id) {
+    document.querySelector('.active')?.classList.remove('active');
+    document.getElementById(id).classList.add('active');
+}
 
 function Nav(props) {
     return (
         <aside className='menu'>
-            <h1>Nav</h1>
+            <header className='user user-connected'>
+                <div className='info-user'>
+                    <img src={userImg} alt="User" />
+                    <h1>Nickname</h1>
+                </div>
+                <div className='status'></div>
+            </header>
+
+            <section>
+                <div className='user' id='user1' onClick={() => activeUser('user1')}>
+                    <h2>User 1</h2>
+                    <div className='status'></div>
+                </div>
+                <div className='user' id='user2' onClick={() => activeUser('user2')}>
+                    <h2>User 2</h2>
+                    <div className='status'></div>
+                </div>
+            </section>
         </aside>
     )
 }
